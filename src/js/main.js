d76b8a53545e07e2ae99babde5c21d412d077ba1
Krejdo <krejdo2011@gmail.com>
2021-03-20 22:30:55 +0500

    Like dislike btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 import 'jquery';
 import '../scss/main.scss';
 import '../pug/index.pug';
+<<<<<<< HEAD
+=======
+import './vendor/like-dislike'
+import './vendor/star-rating' //Не подключается в конфиге, исправить, отсюда убрать
+>>>>>>> 3ed908a... Getting started on card room
 //import '../pug/page.pug';
