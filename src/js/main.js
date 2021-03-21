d497e5bc6413bfd01b20593cb369a6b1ac492c11
Krejdo <krejdo2011@gmail.com>
2021-03-22 00:10:00 +0500

    create range slider (not finished)

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,6 @@ import '../scss/main.scss';
 import '../pug/index.pug';
 import './vendor/like-dislike'
 import './vendor/rateBtn'
+import './vendor/slider'
+import './vendor/sliderTuchPunch'
 //import '../pug/page.pug';
