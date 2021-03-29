7fe363e0b1a59acac88cf0b1e56585d3f14d44f8
Krejdo(job) <krejdo2011@gmail.com>
2021-03-29 19:00:39 +0500

    work on dropdown

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
+$(function() {
     // on click on  setting button
-    $(".button").click(function() {
+    $(".dropdown__button").click(function() {
         var val = $(this).attr('id');
         if (val == 1) {
-            $("ul").hide();
+            $(".dropdown__list").hide();
             $(this).attr('id', '0');
         } else {
-            $("ul").show();
+            $(".dropdown__list").show();
             $(this).attr('id', '1');
         }
 
     });
 
     //Mouse click on setting button and ul list
-    $("ul, .button").mouseup(function() {
+    $(".dropdown__list, .dropdown__button").mouseup(function() {
         return false;
     });
 
     //Document Click
     $(document).mouseup(function() {
-        $("ul").hide();
-        $(".button").attr('id', '0');
+        $(".dropdown__list").hide();
+        $(".dropdown__button").attr('id', '0');
     });
 });
 
+//=================================================================================//
 
-
-$(document).ready(function() {
+$(function() {
     $('.num-in span').click(function () {
         var $input = $(this).parents('.num-block').find('input.in-num');
       if($(this).hasClass('minus')) {
         var count = parseFloat($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        if (count < 2) {
+        count = count < 0 ? 0 : count;
+        if (count < 1) {
           $(this).addClass('dis');
         }
         else {
@@ -43,13 +43,28 @@ $(document).ready(function() {
       else {
         var count = parseFloat($input.val()) + 1
         $input.val(count);
-        if (count > 1) {
+        if (count > 0) {
           $(this).parents('.num-block').find(('.minus')).removeClass('dis');
         }
       }
+
+
+      var bed = parseFloat($input.val());
+      const bedroom = ["спальня", "спальни", "спален"]
+
+      if (bed == 1) {
+        $('#values').text( bed + " " + bedroom[0]);
+      }
+      else if (bed >= 2 && bed <= 4) {
+        $('#values').text( bed + " " + bedroom[1]);
+      }
+      else if (bed >= 5) {
+        $('#values').text( bed + " " + bedroom[2]);
+      }
       
-      $input.change();
-      return false;
+
+      // $input.change();
+      // return false;
     });
     
   });
